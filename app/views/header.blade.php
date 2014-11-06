614f03eff98f416fed1e1603aefea3205121a4e9
Cyriaque Skrapits <cysoun@gmail.com>
2014-11-06 15:31:50 +0100

    Sign in page + fixed some stuff in the main CSS

--- app/views/header.blade.php
+++ app/views/header.blade.php
@@ -24,7 +24,12 @@
 				</div>
 			</form>
 			<ul class="nav navbar-nav navbar-right">
-				<li class=""><a href="signin"><span class="glyphicon glyphicon-ok-circle"></span> Sign in</a></li>
+				<li id="header-signin">
+					<a href="signin"><span class="glyphicon glyphicon-ok-circle"></span> Sign in</a>
+					<div id="header-signin-panel">
+						@include('fragments/signin-form')
+					</div>
+				</li>
 				<li class=""><a href="signup"><span class="glyphicon glyphicon-upload"></span> Sign up</a></li>
 			</ul>
 		</div><!--/.nav-collapse -->
---
+++ app/views/signin.blade.php
@@ -0,0 +1,11 @@
+<div id="signin" class="container-fluid page-default">
+	<div class="container">
+		<div class="col-md-4">
+			<img src="img/signin.png" alt="sign in" class="img-responsive">
+		</div>
+		<div class="col-md-8">
+			<h1>Welcome back !</h1>
+			@include('fragments/signin-form')
+		</div>
+	</div>
+</div>
