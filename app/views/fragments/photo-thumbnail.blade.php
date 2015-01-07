92c4ba5c2dbf47fc995d92d5050ee1918a76d5eb
Cyriaque Skrapits <cysoun@gmail.com>
2015-01-07 22:02:01 +0100

    Updated photo thumbnail

--- app/views/fragments/photo-thumbnail.blade.php
+++ app/views/fragments/photo-thumbnail.blade.php
@@ -1,18 +1,20 @@
 <?php
-$image = Image::find($id);
-$id = $image->id;
-$title = $image->name;
-$path = $image->path();
-$avatar = asset('img/avatar.png');
+	$image = Image::find($id);
+	$id = $image->id;
+	$title = $image->name;
+	$path = $image->path();
+	$avatar = asset('img/avatar.png');
+	$author = $image->user();
 ?>
 <div class="photo-thumbnail photo">
 	<img src="{{ $path }}" class="img-responsive" alt=""/>
 	<div class="photo-thumbnail-overlay">
-		<div class="photo-thumbnail-title">
-			<a href="/photo/{{ $id }}">
-				<img src="{{ $avatar }}" class="avatar"/>
-				{{ $title }}
-			</a>
+		<div class="photo-thumbnail-infos">
+			<a href="/user/{{ $author->pseudo }}" ><img src="{{ $avatar }}" class="avatar"/></a>
+			<div>
+				<span class="photo-thumbnail-title"><a href="/photo/{{ $id }}" >{{ $title }}</a></span>
+				<span class="photo-thumbnail-author">By <a href="/user/{{ $author->pseudo }}" >{{ $author->name }}</a></span>
+			</div>
 		</div>
 	</div>
 </div>
--- app/views/user/latest.blade.php
+++ app/views/user/latest.blade.php
@@ -5,19 +5,9 @@
 <h1>{{{ trans('pixhub.user-latest-photos-from') }}}</h1>
 <div id="user-latest-grid">
 	<?php
-		/*for ($i = 0; $i < 10; $i++)
-		{
-			$newline = ($i % 4 == 0);
-
-			echo ('<div class="col-lg-12 user-grid-thumbnail">');
-			echo (View::make('fragments/photo-thumbnail', array('title' => 'Photo #' . $i)));
-			echo ('</div>');
-		}*/
-
 		for ($i = 0; $i < count($photos); $i++)
 		{
 			echo ('<div class="col-lg-12 user-grid-thumbnail">');
-			//echo (View::make('fragments/photo-thumbnail', array('title' => $photos[$i]->name, 'avatar' => asset('img/avatar.png'), 'image' => Image::find($photos[$i]->id)->path())));
 			echo (View::make('fragments/photo-thumbnail', array('id' => $photos[$i]->id)));
 			echo ('</div>');
 		}
