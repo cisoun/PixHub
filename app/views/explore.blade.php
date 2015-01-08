62208eab379efde89709908d3e2d2d586514e19d
Cyriaque Skrapits <cysoun@gmail.com>
2015-01-08 03:40:58 +0100

    Enhanced Explore page + fixed text align in photo thumbnails + updated gitignore (mostly for Brackets)

--- app/views/explore.blade.php
+++ app/views/explore.blade.php
@@ -1,18 +1,28 @@
 <?php
-	$images = Image::getRandomImages(5);
+$images = Image::getRandomImages(5);
 ?>
-<div id="explore" class="page container">
+<div id="explore" class="container">
 	<div id="explore-title">
-		<h1>Let's explore some photos the users have uploaded !</h1>
-		<a href="#explore-gallery"><span class="glyphicon glyphicon-chevron-down"></span></a>
+		<div>
+			<img src="{{ asset('img/explore.png') }}"/>
+			<h1>Let's explore some photos the users have uploaded !</h1>
+			<a href="#explore-gallery"><span class="glyphicon glyphicon-chevron-down"></span></a>
+		</div>
 	</div>
-	<a id="explore-gallery"></a>
-	<?php
-		for ($i = 0; $i < count($images); $i++)
-		{
-			echo ('<div class="col-lg-12 user-grid-thumbnail">');
-			echo (View::make('fragments/photo-thumbnail', array('id' => $images[$i]->id)));
-			echo ('</div>');
-		}
-	?>
+	<div id="explore-gallery">
+		@if(count($images) > 0)
+			<div>
+				<?php
+					for ($i = 0; $i < count($images); $i++)
+					{
+						echo ('<div class="col-lg-12 user-grid-thumbnail">');
+						echo (View::make('fragments/photo-thumbnail', array('id' => $images[$i]->id)));
+						echo ('</div>');
+					}
+				?>
+			</div>
+		@else
+			<h1>Awww nothing... :(</h1>
+		@endif
+	</div>
 </div>
--- public/js/pixhub-explore.js
+++ public/js/pixhub-explore.js
@@ -1,3 +1,6 @@
+PixHub.makeTransparentNavbar(true);
+
+// Animation
 $(function() {
 	$('a[href*=#]:not([href=#])').click(function() {
 		if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
