6e07faec01272cd2601b48ce539323d830587465
Cyriaque Skrapits <cysoun@gmail.com>
2015-01-08 11:34:27 +0100

    Fixed a variable from the last commit

--- app/views/fragments/photo-thumbnail.blade.php
+++ app/views/fragments/photo-thumbnail.blade.php
@@ -5,12 +5,12 @@
 	$author = $image->user();
 ?>
 <div class="photo-thumbnail photo">
-	<a href="/photo/{{ $image->id }}" ><img src="{{ $image->path() }}" class="photo-thumbnail-img" class="img-responsive"/></a>
+	<a href="/photo/{{ $id }}" ><img src="{{ $image->path() }}" class="photo-thumbnail-img" class="img-responsive"/></a>
 	<div class="photo-thumbnail-overlay">
 		<div class="photo-thumbnail-infos">
 			<a href="/user/{{ $author->pseudo }}" ><img src="{{ $avatar }}" class="avatar"/></a>
 			<div>
-				<span class="photo-thumbnail-title"><a href="/photo/{{ $image->$id }}" >{{ $image->name }}</a></span>
+				<span class="photo-thumbnail-title"><a href="/photo/{{ $id }}" >{{ $image->name }}</a></span>
 				<span class="photo-thumbnail-author">In <a href="/album/{{ $album->id }}" >{{ $album->name }}</a> by <a href="/user/{{ $author->pseudo }}" >{{ $author->name }}</a></span>
 			</div>
 		</div>
