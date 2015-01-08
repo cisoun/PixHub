8f1ca1b2f0d709b5723ff28ec634a8c826fd1441
Cyriaque Skrapits <cysoun@gmail.com>
2015-01-08 11:32:03 +0100

    Updated photo thumbnails

--- app/views/fragments/photo-thumbnail.blade.php
+++ app/views/fragments/photo-thumbnail.blade.php
@@ -1,19 +1,17 @@
 <?php
 	$image = Image::find($id);
-	$id = $image->id;
-	$title = $image->name;
-	$path = $image->path();
+	$album = $image->album;
 	$avatar = asset('img/avatar.png');
 	$author = $image->user();
 ?>
 <div class="photo-thumbnail photo">
-	<a href="/photo/{{ $id }}" ><img src="{{ $path }}" class="photo-thumbnail-img" class="img-responsive"/></a>
+	<a href="/photo/{{ $image->id }}" ><img src="{{ $image->path() }}" class="photo-thumbnail-img" class="img-responsive"/></a>
 	<div class="photo-thumbnail-overlay">
 		<div class="photo-thumbnail-infos">
 			<a href="/user/{{ $author->pseudo }}" ><img src="{{ $avatar }}" class="avatar"/></a>
 			<div>
-				<span class="photo-thumbnail-title"><a href="/photo/{{ $id }}" >{{ $title }}</a></span>
-				<span class="photo-thumbnail-author">By <a href="/user/{{ $author->pseudo }}" >{{ $author->name }}</a></span>
+				<span class="photo-thumbnail-title"><a href="/photo/{{ $image->$id }}" >{{ $image->name }}</a></span>
+				<span class="photo-thumbnail-author">In <a href="/album/{{ $album->id }}" >{{ $album->name }}</a> by <a href="/user/{{ $author->pseudo }}" >{{ $author->name }}</a></span>
 			</div>
 		</div>
 	</div>
