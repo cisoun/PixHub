09d31fec477df26eb4b7945da6b7a626c4ec2f58
Cyriaque Skrapits <cysoun@gmail.com>
2015-01-09 06:55:23 +0100

    Updated homepage

--- app/views/home.blade.php
+++ app/views/home.blade.php
@@ -23,23 +23,67 @@
 				</p>
 			</div>
 			<div class="col-md-6">
-				<img src="img/background-yellow.jpg" alt="..." class="img-thumbnail">
+				<img src="img/avatar.png" alt="..." class="img-circle pull-right">
 			</div>
 		</div>
+
+		<div class="row big-title">What's cool about it</div>
+
 		<div class="row">
 			<div class="col-md-6">
-				<img src="img/background-red.jpg" alt="..." class="img-thumbnail">
+				<img src="img/home/exif.png" alt="..." class="img-thumbnail">
 			</div>
 			<div class="col-md-6">
-				<h1>What's cool about  it</h1>
-				<ul>
-					<li>Exif support</li>
-					<li>Easy upload manager</li>
-					<li>Social media support</li>
-					<li>Photo management</li>
-					<li>Tag your photos</li>
-				</ul>
+				<h3>EXIF support</h3>
+				<p>PixHub supports EXIF datas so you can share your camera settings.</p>
 			</div>
+		</div>
+
+		<div class="row">
+			<div class="col-md-6">
+				<h3>Easy upload</h3>
+				<p>Upload your pictures easily thanks to the drag'n'drop file uploader</p>
+				<p>Set an album title, put your files in the box and let PixHub do its job !</p>
+			</div>
+			<div class="col-md-6">
+				<img src="img/home/upload.jpg" alt="..." class="img-thumbnail">
+			</div>
+		</div>
+
+		<div class="row">
+			<div class="col-md-6">
+				<img src="img/home/preview.jpg" alt="..." class="img-thumbnail">
+			</div>
+			<div class="col-md-6">
+				<h3>Modern interface</h3>
+				<p>PixHub was designed to be modern and responsive design.</p>
+				<p>It takes advantage of the responsive design and use nice colors to amaze your eyes.</p>
+			</div>
+		</div>
+
+		<div class="row">
+			<div class="col-md-6">
+				<h3>Photo managment</h3>
+				<p>
+					Upload your photos and change their title and description.<br/>
+					You tell the story !
+				</p>
+				<p>
+					It's really easy ! Just click on the field you want to modify,<br/>
+					change the text, and it's done !
+				</p>
+			</div>
+			<div class="col-md-6">
+				<img src="img/home/titles.png" alt="..." class="img-thumbnail">
+			</div>
+		</div>
+
+		<div class="row">
+			<h1>And wait, there's more !</h1>
+			<ul>
+				<li>Social media support</li>
+				<li>Tag your photos</li>
+			</ul>
 		</div>
 	</div>
 </div><!-- /.container -->
